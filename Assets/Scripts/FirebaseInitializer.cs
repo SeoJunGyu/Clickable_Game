c329541325a74f68df16f4264fb1db3a10fb3cba
서준규 <thddlwnsrb@naver.com>
2025-10-24 13:11:37 +0900

    FirstCommit

---
+++ Assets/Scripts/FirebaseInitializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Firebase;
+using Cysharp.Threading.Tasks;
+
+//파이어 베이스 초기화 클래스
+public class FirebaseInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
